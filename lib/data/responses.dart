117fafec06520d7d7f73975243b2dc21fff427be
Ahmad jz <ahmadjzz99@gmail.com>
2023-02-01 12:24:39 +0300

    finish responses classes
    create the responses classes and implement it using json_serializable

---
+++ lib/data/responses.dart
@@ -0,0 +1,70 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:json_annotation/json_annotation.dart';
+
+part 'responses.g.dart';
+
+@JsonSerializable()
+class BaseResponse {
+  @JsonKey(name: "status")
+  int? status;
+  @JsonKey(name: "message")
+  String? message;
+}
+
+@JsonSerializable()
+class CustomerResponse {
+  @JsonKey(name: "id")
+  int? id;
+  @JsonKey(name: "numOfNotification")
+  int? numOfNotification;
+  @JsonKey(name: "name")
+  String? name;
+  CustomerResponse({
+    this.id,
+    this.numOfNotification,
+    this.name,
+  });
+
+  factory CustomerResponse.fromJson(Map<String, dynamic> json) =>
+      _$CustomerResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$CustomerResponseToJson(this);
+}
+
+@JsonSerializable()
+class ContactsResponse {
+  @JsonKey(name: "phone")
+  String? phone;
+  @JsonKey(name: "email")
+  String? email;
+  @JsonKey(name: "link")
+  String? link;
+  ContactsResponse({
+    this.phone,
+    this.email,
+    this.link,
+  });
+
+  factory ContactsResponse.fromJson(Map<String, dynamic> json) =>
+      _$ContactsResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$ContactsResponseToJson(this);
+}
+
+@JsonSerializable()
+class AuthenticationResponse extends BaseResponse {
+  // the main class we are going to use
+  @JsonKey(name: "customer")
+  CustomerResponse? customerResponse;
+  @JsonKey(name: "contacts")
+  ContactsResponse? contactsResponse;
+  AuthenticationResponse({
+    this.customerResponse,
+    this.contactsResponse,
+  });
+
+  factory AuthenticationResponse.fromJson(Map<String, dynamic> json) =>
+      _$AuthenticationResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AuthenticationResponseToJson(this);
+}
---
+++ lib/data/responses.g.dart
@@ -0,0 +1,67 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'responses.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BaseResponse _$BaseResponseFromJson(Map<String, dynamic> json) => BaseResponse()
+  ..status = json['status'] as int?
+  ..message = json['message'] as String?;
+
+Map<String, dynamic> _$BaseResponseToJson(BaseResponse instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+    };
+
+CustomerResponse _$CustomerResponseFromJson(Map<String, dynamic> json) =>
+    CustomerResponse(
+      id: json['id'] as int?,
+      numOfNotification: json['numOfNotification'] as int?,
+      name: json['name'] as String?,
+    );
+
+Map<String, dynamic> _$CustomerResponseToJson(CustomerResponse instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'numOfNotification': instance.numOfNotification,
+      'name': instance.name,
+    };
+
+ContactsResponse _$ContactsResponseFromJson(Map<String, dynamic> json) =>
+    ContactsResponse(
+      phone: json['phone'] as String?,
+      email: json['email'] as String?,
+      link: json['link'] as String?,
+    );
+
+Map<String, dynamic> _$ContactsResponseToJson(ContactsResponse instance) =>
+    <String, dynamic>{
+      'phone': instance.phone,
+      'email': instance.email,
+      'link': instance.link,
+    };
+
+AuthenticationResponse _$AuthenticationResponseFromJson(
+        Map<String, dynamic> json) =>
+    AuthenticationResponse(
+      customerResponse: json['customer'] == null
+          ? null
+          : CustomerResponse.fromJson(json['customer'] as Map<String, dynamic>),
+      contactsResponse: json['contacts'] == null
+          ? null
+          : ContactsResponse.fromJson(json['contacts'] as Map<String, dynamic>),
+    )
+      ..status = json['status'] as int?
+      ..message = json['message'] as String?;
+
+Map<String, dynamic> _$AuthenticationResponseToJson(
+        AuthenticationResponse instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+      'customer': instance.customerResponse,
+      'contacts': instance.contactsResponse,
+    };
