eb70b01239a9648c86dcda83304e4eb6ef7c1bd4
Ahmad jz <ahmadjzz99@gmail.com>
2023-02-07 11:34:13 +0300

    create dio factory with our options

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,5 +1,7 @@
 class Constants {
   static const String baseURL = "http://localhost:8000/api";
-  static const String empty = "http://localhost:8000/api";
+  static const String empty = "";
+  static const String token = "SEND TOKEN HERE";
   static const int zero = 0;
+  static const int apiTimeOut = 60000;
 }
--- lib/data/network/dio_factory.dart
+++ lib/data/network/dio_factory.dart
@@ -1,4 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:shop_app_mvvm/app/app_prefs.dart';
 import 'package:shop_app_mvvm/app/constants.dart';
 
 const String applicationJson = "application/json";
@@ -8,22 +11,35 @@ const String authorization = "authorization";
 const String defaultLanguage = "language";
 
 class DioFactory {
+  final AppPreferences _appPreferences;
+  DioFactory(this._appPreferences);
+
   Future<Dio> getDio() async {
     Dio dio = Dio();
 
-    int timeOut = 60 * 1000; // a min time out
+    String language = await _appPreferences.getAppLanguage();
     Map<String, String> headers = {
       contentType: applicationJson,
       accept: applicationJson,
-      authorization: "SEND TOKEN HERE",
-      defaultLanguage: "en" // todo get lang from app prefs
+      authorization: Constants.token,
+      defaultLanguage: language
     };
 
     dio.options = BaseOptions(
         baseUrl: Constants.baseURL,
         headers: headers,
-        receiveTimeout: timeOut,
-        sendTimeout: timeOut);
+        receiveTimeout: Constants.apiTimeOut,
+        sendTimeout: Constants.apiTimeOut);
+
+    if (!kReleaseMode) {
+      dio.interceptors.add(
+        PrettyDioLogger(
+          responseHeader: true,
+          requestBody: true,
+          requestHeader: true,
+        ),
+      );
+    }
 
     return dio;
   }
